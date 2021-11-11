62398a80c1e971824f1c1c3a9edbf42d5ffe1fa3
samraat121 <geostruc.samraat@gmail.com>
2021-11-11 15:28:55 +0600

    Event handleling in react js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import CC from './CC'
 
 function App() {
+
+  const shoot = () => {
+    alert("Great Shot!");
+  }
+  const callAlert =() => {
+    alert("Hello world");
+  }
+  const inputEvent = () => {
+    alert("This is call imput event");
+  }
+  const mouseEnter = () => {
+    alert("Hello this is mouse enter effect");
+  }
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <h1>Event Handling in React js</h1>
+      <button onClick={shoot}>Take the shot!</button><br/>
+      <button onClick={callAlert}>CLick and Change</button><br/>
+      <input type="text" className="form-control mb-2 mr-sm-2" onChange={inputEvent} placeholder="Jane Doe" />
+      <p onMouseOver={mouseEnter}>This is Mouse Hover effect</p> <br/>
+
+      <CC />
+    
     </div>
   );
 }
---
+++ src/CC.js
@@ -0,0 +1,18 @@
+import React,{Component} from 'react'
+class CC extends React.Component {
+    classComponent = () => {
+        alert("This is is Class COmponent Here");
+    }
+    inPutChange = () => {
+        alert('Input file change');
+    }
+    render() {
+        return (
+             <div>
+                 <button onClick={this.classComponent}>Click now Class component</button>
+                 <input type="text" onChange={this.inPutChange} className="form-control mb-2 mr-sm-2" placeholder="Jane Doe" />
+             </div>
+        );
+    }
+}
+export default CC;
