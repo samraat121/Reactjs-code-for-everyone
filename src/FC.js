1ffee998b21b08ed5a441de8025ce5931829e7ae
samraat121 <geostruc.samraat@gmail.com>
2021-11-11 20:51:25 +0600

    useEffect by w3school timer

--- src/FC.js
+++ src/FC.js
@@ -4,6 +4,7 @@ function FC() {
     const [player, setPlayer] = useState('Tamim Iqbal')
     const [run, setRun] = useState('56')
     const [language, setLanguage] = useState('JavaScript')
+    const [count, setCount] = useState(0)
 
     function changePlayer() {
         setPlayer('Shakib al Hasan')
@@ -12,6 +13,11 @@ function FC() {
     useEffect(() => {
         setLanguage('Python')
     }, [])
+    useEffect(() => {
+        setTimeout(() => {
+            setCount((count) => count+1);
+        }, 1000);
+    })
 
     
     return(
@@ -19,6 +25,7 @@ function FC() {
             <h1>Bangladesh best cricket player is {player}</h1>
             <h2>Bangladesh avarage run rate is 56 but he won by {run}</h2>
             <h2>The Declare Language is  {language}</h2>
+            <h1>Your Time Start Now, Now you count your time {count}</h1>
             <button onClick={changePlayer}>Click to change cricket player</button>
         </div>
     )
