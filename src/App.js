ebd1823f2934cfe5f6d2ca36e770dc2e8e6f251e
samraat121 <geostruc.samraat@gmail.com>
2021-11-14 17:53:58 +0600

    Bootstrap init

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
-import FC from './FC'
-import Home from './Home'
 
 function App() {
   return (
     <div className="App">
+      <h1 className="text-center">React js with Core Learners</h1> <hr />
       
-      <h1>Hooks in React js</h1><hr/>
-      <FC />
-      <hr/>
-      <Home />
+      
     
     </div>
   );
