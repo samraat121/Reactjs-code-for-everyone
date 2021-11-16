fdfdc934e33a3e6bc90a7d947ac8f2fa117d7144
samraat121 <geostruc.samraat@gmail.com>
2021-11-16 14:45:39 +0600

    React Router

--- src/App.js
+++ src/App.js
@@ -3,16 +3,45 @@ import './App.css';
 import FC from './FC';
 import Login from './Login';
 import Todos from './Todos';
+import Navbar from './Navbar'
+import Home from './pages/Home'
+import Contact from './pages/Contact'
+import Services from './pages/Services'
+import About from './pages/About'
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <h1 className="text-center">React js with Core Learners</h1> <hr />
-      {/* <FC /> */}
-      {/* <Login /> */}
-      <Todos />
-    
-    </div>
+    <Router>
+      <div className="App">
+      <Navbar />
+
+      <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+
+          <Route exact path="/about">
+            <About />
+          </Route>
+
+          <Route exact path="/contact">
+            <Contact />
+          </Route>
+
+          <Route exact path="/services">
+            <Services />
+          </Route>
+
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
