d90a66ea322384415c43275d9b69dff4583c9405
samraat121 <geostruc.samraat@gmail.com>
2021-11-16 18:51:53 +0600

    Local storage in react js

--- src/Localstorage.js
+++ src/Localstorage.js
@@ -1,20 +1,34 @@
 import React,{useState} from 'react'
 
 function Localstorage() {
-    const [name, setName] = useState('')
-    const submitForm = () => {
-    
-       localStorage.setItem('name', name)
+    const [language, setLanguage] = useState('')
+    const [framework, setFramework] = useState('')
+
+    const saveLanguage = () => {
+        var courses = JSON.parse(localStorage.getItem('courses', ) || '[]')
+        var course = {
+            language: language,
+            framework: framework,
+        }
+        courses.push(course)
+        localStorage.setItem('courses', JSON.stringify(courses));
+        
     }
+
     return (
         <div className="container">
             <h1 className="text-center">React js Local Storage</h1>
             <div className="form-group">
-              <label htmlFor="name">Name</label>
+              <label htmlFor="language">Language</label>
               <input type="text"
-                className="form-control" name="name" id="name" onChange={(e)=>{setName(e.target.value)}} value={name} aria-describedby="" placeholder="name" /> 
+                className="form-control" name="language" value={language} onChange={(e)=>{setLanguage(e.target.value)}} id="language" placeholder="language" />
             </div>
-            <button type="submit" onClick={submitForm} className="btn btn-primary">Storage</button><hr />
+            <div className="form-group">
+              <label htmlFor="framework">framework</label>
+              <input type="text"
+                className="form-control" name="framework" value={framework} onChange={(e)=>{setFramework(e.target.value)}} id="framework" placeholder="framework" />
+            </div>
+            <button type="submit" onClick={saveLanguage} className="btn btn-primary">Submit</button>
             <h4>Our Local Storage Value {localStorage.getItem('name')}</h4>
         </div>
     )
